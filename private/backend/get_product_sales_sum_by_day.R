c5dc6d8cae6e3d1223430557fd1db6dc890e3bbd
naroarnold <ctenochaetus@gmail.com>
2015-11-24 00:31:29 -0500

    Third commit

--- private/backend/get_product_sales_sum_by_day.R
+++ private/backend/get_product_sales_sum_by_day.R
@@ -1,8 +1,10 @@
 get_p_daily_sales_avg <- function(product_id,con)
 {
+  #find overall sales of a given product
 	query <- paste("SELECT store_sales,day_of_month,month_of_year FROM sales_fact_dec_1998_p,time_by_day WHERE product_id = ",product_id,"AND sales_fact_dec_1998_p.time_id = time_by_day.time_id;")
-	data <- dbGetQuery(con,query)
+  data <- dbGetQuery(con,query)
 
+  #calculate the mean
 	result <- mean(data$store_sales)
 	return (result)
 }
