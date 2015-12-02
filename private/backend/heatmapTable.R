ab33e0f8a26b4991bd184bf1bccd0f5c839a4069
naroarnold <ctenochaetus@gmail.com>
2015-12-02 12:25:54 -0500

    Third commit

--- private/backend/heatmapTable.R
+++ private/backend/heatmapTable.R
@@ -1,15 +1,10 @@
 library(plyr)
 
-####TO DO: CREATE DATA TABLE FOR INPUT TO THIS FUNCTION
-####COLUMN 1: Product ID
-####COLUMN 2: Product department as factor
-####COLUMN 3: Projected sales for product (raw)
-####COLUMN 4: Projected sales for product (proportionate to mean)
-
-##column names: product_id, product_department, raw_sales, prop_sales
-#column names tentative
 heatmapTable <- function(intable) {
+  #convert department names to factor (in order to operate by department)
   intable$product_department <- as.factor(intable$product_department)
+  
+  #create a table that assigns a weight to each department's total forecast
   heattable <- ddply(intable, "product_department", summarize, 
                      traffic = sum(raw_forecast)/sum(mean_observed))
   
