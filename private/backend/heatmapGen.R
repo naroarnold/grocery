a88a5ec4bd1b9af10f3f5514aaf72733d1ad16c9
naroarnold <ctenochaetus@gmail.com>
2015-12-02 12:22:33 -0500

    Third commit
    Inverts heat map correctly, takes file name as input

--- private/backend/heatmapGen.R
+++ private/backend/heatmapGen.R
@@ -1,32 +1,31 @@
-heatmapGen <- function(heattable) {
-  y <- attr(heatmapGen, "name")
-  if(is.null(y)) {y<-0}
-  y <- y+1
-
+heatmapGen <- function(heattable, outname) {
+  
+  
+  #convert the factor value from the input table back to a character
   heattable[[1]] <- lapply(heattable[[1]], as.character)
-
+  
   #input floor plan
   #this must be set to the specific floor plan for this iteration of the module
-  # filename <- "~/uky 15-16/CS499/project_code/FloorPlan.csv"
+
   filename <- file.path(mw.directory, "FloorPlan.csv")
   floorplan <- read.csv(file=filename, stringsAsFactors=FALSE)
-
-
+  floorplan <- floorplan[rev(rownames(floorplan)),]
+  rownames(floorplan) <- 1:length(rownames(floorplan))
+  
   ##map department weight to department location in store
   floorweight <- as.matrix(as.data.frame(lapply(floorplan, FUN=function(x,y) {
     sapply(x, FUN=function(x,y) {y [y[[1]]==x, 2]}, y)
   }, heattable)))
-
-  img_filename <- gettextf("heatmap_%d.jpg", y);
-
+  
+  
   #create heatmap
-  # jpeg(file = paste("heatmap_", y, ".jpg", sep=""))
-  jpeg(file = file.path(mw.imgDirectory, img_filename))
+    #open file location
+  jpeg(file = file.path(mw.imgDirectory, outname))
+    #establish the heatmap
   store_heatmap <- heatmap(floorweight, Rowv = NA, Colv = NA,
-                           col=rainbow(128, start=0, end=4/6),
+                           col=rainbow(128, start=4/6, end=0),
                            scale = "none", margins = c(5,5))
+    #close file connection
   dev.off()
-  attr(heatmapGen, "name") <<- y
-
-  return (img_filename);
+  
 }
